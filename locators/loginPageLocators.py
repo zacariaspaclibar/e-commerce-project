505c7c73a97a256b63734333083383ddd2e31ab6
zacariaspaclibar <zacariaspaclibar@gmail.com>
2026-09-03 12:19:00 +0800

    refactor/login page locators

--- locators/loginPageLocators.py
+++ locators/loginPageLocators.py
@@ -1,6 +1,9 @@
 from playwright.sync_api import Page
 class LoginPageLocators:
     def __init__(self,page):
+        # Buttons
+        self.login_btn = page.get_by_role('button',name='Login')
+        
+        #Fields 
         self.email_field = page.get_by_placeholder('email@example.com')
         self.password_field = page.get_by_placeholder('enter your passsword')
-        self.login_btn = page.get_by_role('button',name='Login')
