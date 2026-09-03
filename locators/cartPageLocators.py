7a92d4b01f202b21b46280371ab151ee4915ea8f
zacariaspaclibar <zacariaspaclibar@gmail.com>
2026-09-03 15:51:56 +0800

    refactor/minor changes on the total and subtotal locator

---
+++ locators/cartPageLocators.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import Page
+
+
+class CartPageLocators:
+    def __init__(self,page:Page):
+        self.continue_shopping_btn = page.get_by_role('button',name='Continue Shopping')
+        self.buy_now_btn = page.get_by_role('button',name='Buy Now')
+        self.delete_btn = page.locator('.btn btn-danger')
+        self.checkout_btn = page.get_by_role('button',name='Checkout')
+        self.total = (
+    page.locator("li.totalRow")
+    .filter(has=page.locator("span.label", has_text="Total"))
+    .locator("span.value")
+)
+        self.subtotal = (
+    page.locator("li.totalRow")
+    .filter(has=page.locator("span.label", has_text="Subtotal"))
+    .locator("span.value")
+)
