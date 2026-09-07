5cee9266538a6edc77720b6c940c6a7bc3a73846
zacariaspaclibar <zacariaspaclibar@gmail.com>
2026-09-08 00:16:52 +0800

    adding the negative scenarios

--- locators/registerPageLocators.py
+++ locators/registerPageLocators.py
@@ -6,7 +6,7 @@ class RegisterPageLocators:
     def __init__(self,page:Page):
          # Alert
         self.success_alert = page.get_by_text("Registered Successfully")
-        self.error_alert = page.get_by_role("alert", name="User already exisits with")
+        self.error_alert = page.get_by_text("User already exisits with")
         
         #Heading
         self.account_successfully_created_text = page.get_by_role("heading", name="Account Created Successfully")
@@ -31,4 +31,11 @@ class RegisterPageLocators:
         self.occupation = page.get_by_role("combobox")
         self.gender = page.get_by_role("radio", name="Male", exact=True)
 
-       
+        #Required field - Error message
+        self.password_not_match_text = page.get_by_text("Password and Confirm Password must match with each other.")
+        self.first_name_required_text = page.get_by_text("*First Name is required")
+        self.email_required_text = page.get_by_text("*Email is required")
+        self.phone_number_required_text = page.get_by_text("*Phone Number is required")
+        self.password_required_text = page.get_by_text("*Password is required")
+        self.confirm_password_required_text = page.get_by_text("Confirm Password is required")
+        self.checkbox_required_text = page.get_by_text("*Please check above checkbox")
--- tests/test_accountRegistration.py
+++ tests/test_accountRegistration.py
@@ -42,6 +42,35 @@ def test_email_already_exist(browser_init):
     registerLocators.register_page_btn.click()
     expect(registerLocators.error_alert).to_be_visible()
 
+def test_password_not_match(browser_init):
+    loginPage = LoginPage(browser_init)
+    loginPage.navigate()
+    registerLocators = RegisterPageLocators(browser_init)
+    registerLocators.register_btn.click()
+    registerLocators.firstName_field.fill(credentials.FIRSTNAME)
+    registerLocators.lastName_field.fill(credentials.LASTNAME)
+    registerLocators.email_field.fill(credentials.EMAIL)
+    registerLocators.phone_field.fill(credentials.PHONE_NUMBER)
+    registerLocators.occupation.select_option("2: Student")
+    registerLocators.gender.check()
+    registerLocators.password_field.fill(credentials.PASSWORD)
+    registerLocators.confirm_password_field.fill(credentials.INCORRECT_CONFIRM_PASSWORD)
+    registerLocators.age_checkbox.click()
+    registerLocators.register_page_btn.click()
+    expect(registerLocators.password_not_match_text).to_be_visible()
+
+def test_empty_field(browser_init):
+    loginPage = LoginPage(browser_init)
+    loginPage.navigate()
+    registerLocators = RegisterPageLocators(browser_init)
+    registerLocators.register_btn.click()
+    registerLocators.register_page_btn.click()
+    expect(registerLocators.first_name_required_text).to_be_visible()
+    expect(registerLocators.email_required_text).to_be_visible()
+    expect(registerLocators.phone_number_required_text).to_be_visible()
+    expect(registerLocators.password_required_text).to_be_visible()
+    expect(registerLocators.confirm_password_required_text).to_be_visible()
+    expect(registerLocators.checkbox_required_text).to_be_visible()
 
 
 
