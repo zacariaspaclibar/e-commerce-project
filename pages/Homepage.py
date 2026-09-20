aff628912ffbc9f1383aa25872f25bd4cb54bb84
zacariaspaclibar <zacariaspaclibar@gmail.com>
2026-09-20 21:12:31 +0800

    adding the green banner test case

--- pages/Homepage.py
+++ pages/Homepage.py
@@ -1,13 +1,17 @@
-from playwright.async_api import expect
+from playwright.async_api import Page, expect
 
 from locators.homePageLocators import HomePageLocators
 
 
 class HomePage:
-    def __init__(self,page):
+    def __init__(self,page:Page):
         self.page = page
         self.home_page_locators = HomePageLocators(self.page)
 
     def verify_green_banner(self):
-        self.home_page_locators.blinking_green_btn.click()
-        expect(self.page).to_have_title("QA Career Accelerator | Get Hired in 90 Days | Rahul Shetty")        
+        with self.page.context.expect_page() as new_page:
+            self.home_page_locators.blinking_green_btn.click()
+            new_tab = new_page.value
+            new_tab.wait_for_load_state()
+            assert "QA Career Accelerator | Get Hired in 90 Days | Rahul Shetty" in new_tab.title()#working
+            # expect(new_tab).to_have_title("QA Career Accelerator | Get Hired in 90 Days | Rahul Shetty") - now working 
