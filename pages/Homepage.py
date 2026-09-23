5046741a856ebb7c8cacd400d5f281ef11829a6d
zacariaspaclibar <zacariaspaclibar@gmail.com>
2026-09-23 15:26:26 +0800

    Feature/filter product name (#4)
    * feature/modify green banner and add authenticated page
    
    * Add filter by name and price range test case

--- pages/Homepage.py
+++ pages/Homepage.py
@@ -1,7 +1,6 @@
 from playwright.sync_api import expect
-
 from locators.homePageLocators import HomePageLocators
-
+import re
 
 class HomePage:
     def __init__(self,page):
@@ -15,3 +14,26 @@ class HomePage:
         new_tab.wait_for_load_state()
         header = new_tab.get_by_text('From QA Learner to Hired Professional in 90 Days')
         expect(header).to_be_visible()
+                    
+    def filter_by_name(self,product_name):
+        self.home_page_locators.search_field.fill(product_name)
+        self.home_page_locators.search_field.press('Enter')
+        text = self.home_page_locators.show_result.text_content()
+        self.verify_product_exist(text)
+    
+    def filter_by_price(self,price_range_min,price_range_max):
+        self.home_page_locators.min_price_range_field.fill(price_range_min)
+        self.home_page_locators.max_price_range_field.fill(price_range_max)
+        self.home_page_locators.max_price_range_field.press('Enter')
+        text = self.home_page_locators.show_result.text_content()
+        self.verify_product_exist(text)
+        
+    def verify_product_exist(self,text):
+        count = re.search(r'\d+',text)
+        result_count = int(count.group())
+        if result_count > 0:
+            expect(self.home_page_locators.show_result).to_have_text(f'Showing {result_count} results   | ')
+        else: 
+            expect(self.home_page_locators.error_product_not_found).to_be_visible()
+            
+        
--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,6 +1,31 @@
+import pytest
 from pages.Homepage import HomePage
 
 def test_green_banner(authenticated_page):
     home_page = HomePage(authenticated_page)
     home_page.verify_green_banner()
+    
+@pytest.mark.parametrize(
+    'product_name',[
+        'ADIDAS',
+        'ZARA',
+        'iphone',
+        'adidas'
+    ])
+def test_filter_by_name(authenticated_page,product_name):
+    home_page = HomePage(authenticated_page)
+    home_page.filter_by_name(product_name)
+
+@pytest.mark.parametrize('price_range_min, price_range_max',[
+    ('11000','11499'),
+    ('11000','11500'),
+    ('11500','55000'),
+    ('30000','55000'),
+    ('55001','60000')
+])
+def test_filter_by_price(authenticated_page,price_range_min,price_range_max):
+    home_page = HomePage(authenticated_page)
+    home_page.filter_by_price(price_range_min,price_range_max)
+    
+    
     
